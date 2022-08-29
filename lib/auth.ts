0613927363f7360eeef9397d0624a334d5c80392
shivam123425 <shivam123425@gmail.com>
2022-08-29 23:01:06 +0530

    Create validateRoute middleware hof and me route

---
+++ lib/auth.ts
@@ -0,0 +1,28 @@
+import jwt from "jsonwebtoken";
+import { NextApiRequest, NextApiResponse } from "next";
+import prisma from "./prisma";
+
+export const validateRoute = (handler) => {
+  return async (req: NextApiRequest, res: NextApiResponse) => {
+    const token = req.cookies[process.env.ACCESS_TOKEN_KEY];
+
+    if (!token) {
+      res.status(401);
+      return res.json({ error: "Not Authorized" });
+    }
+
+    try {
+      const { id } = jwt.verify(token, process.env.JWT_SECRET);
+      const user = await prisma.user.findUnique({
+        where: { id },
+      });
+      if (!user) {
+        throw new Error("User not found");
+      }
+      return handler(req, res, user);
+    } catch (error) {
+      res.status(401);
+      return res.json({ error: "Not Authorized" });
+    }
+  };
+};

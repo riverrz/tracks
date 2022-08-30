3542cd60ca96452dcb9ae11b69285c613c6188bb
shivam123425 <shivam123425@gmail.com>
2022-08-30 23:29:40 +0530

    Create separate validateToken method

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,6 +2,11 @@ import jwt from "jsonwebtoken";
 import { NextApiRequest, NextApiResponse } from "next";
 import prisma from "./prisma";
 
+export const validateToken = (token) => {
+  const user = jwt.verify(token, process.env.JWT_SECRET);
+  return user;
+};
+
 export const validateRoute = (handler) => {
   return async (req: NextApiRequest, res: NextApiResponse) => {
     const token = req.cookies[process.env.ACCESS_TOKEN_KEY];
@@ -12,7 +17,7 @@ export const validateRoute = (handler) => {
     }
 
     try {
-      const { id } = jwt.verify(token, process.env.JWT_SECRET);
+      const { id } = validateToken(token);
       const user = await prisma.user.findUnique({
         where: { id },
       });

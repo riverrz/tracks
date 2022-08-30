0801e21ae20bff515278efe04b9a7bbdb57fdb79
shivam123425 <shivam123425@gmail.com>
2022-08-30 22:06:55 +0530

    Add gradient layout and use it in home page

---
+++ components/layouts/gradient.layout.tsx
@@ -0,0 +1,45 @@
+import { Box, Flex, Text } from "@chakra-ui/layout";
+import { Image } from "@chakra-ui/react";
+import { ReactNode } from "react";
+
+interface Props {
+  color: string;
+  children: ReactNode;
+  image: string;
+  subtitle: string;
+  title: string;
+  description: string;
+  roundImage: boolean;
+}
+
+const GradientLayout = (props: Props) => {
+  const { color, image, roundImage, subtitle, children, title, description } =
+    props;
+  return (
+    <Box
+      height="100%"
+      overflowY="scroll"
+      bgGradient={`linear(${color}.500 0%, ${color}.600 15%, ${color}.700 40%, rgba(0,0,0,0.95) 75%)`}
+    >
+      <Flex bg={`${color}.600`} padding="40px" align="flex-end">
+        <Box padding="20px">
+          <Image
+            boxSize="160px"
+            boxShadow="2xl"
+            src={image}
+            borderRadius={roundImage ? "100%" : "3px"}
+          />
+        </Box>
+        <Box padding="20px" lineHeight="40px" color="white">
+          <Text fontSize="x-small" fontWeight="bold" casing="uppercase">
+            {subtitle}
+          </Text>
+          <Text fontSize="6xl">{title}</Text>
+          <Text fontSize="x-small">{description}</Text>
+        </Box>
+      </Flex>
+    </Box>
+  );
+};
+
+export default GradientLayout;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,21 @@
 import Head from "next/head";
 import Image from "next/image";
+import GradientLayout from "../components/layouts/gradient.layout";
 import styles from "../styles/Home.module.css";
 
 const Home = () => {
-  return <div>home</div>;
+  return (
+    <GradientLayout
+      color="gray"
+      subtitle="Profile"
+      title="Shivam Kumar"
+      description="10 public playlists"
+      image="/me.png"
+      roundImage
+    >
+      <div>Home page</div>
+    </GradientLayout>
+  );
 };
 
 export default Home;
